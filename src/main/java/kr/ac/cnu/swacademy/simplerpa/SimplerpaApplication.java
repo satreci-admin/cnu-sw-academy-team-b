2b528a4489f2faf80d9f30258e389fa0637b7030
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-09-27 20:24:25 +0900

    initial commit

---
+++ src/main/java/kr/ac/cnu/swacademy/simplerpa/SimplerpaApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.cnu.swacademy.simplerpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplerpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplerpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/cnu/swacademy/simplerpa/SimplerpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cnu.swacademy.simplerpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
