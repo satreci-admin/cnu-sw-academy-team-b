a30bf9cc2eb36329c0d219c9e0e46275aab44872
pum005 <108645121+pum005@users.noreply.github.com>
2022-09-28 15:28:18 +0900

    feat : db 초기화 설정 (#1)
    * feat : mysql 연결 설정
    
    * feat : database, user, table 생성, sql 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+create database simpleRPA default character set utf8 collate utf8_general_ci;
+show databases;
+create user 'simpleRPA'@'%' identified by '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'simpleRPA'@'%';
+
+CREATE TABLE robots
+(
+    robot_id   bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    address    VARCHAR(30) NOT NULL,
+    user       VARCHAR(30) NOT NULL,
+    password   VARCHAR(30)  NOT NULL,
+    created_at   timestamp(6) NOT NULL,
+    updated_at   timestamp(6)  DEFAULT NULL
+);
+
+CREATE TABLE job_lists
+(
+    joblist_id  bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(30)  NOT NULL,
+    robot_id      bigint NOT NULL,
+    repeat     TINYINT NOT NULL,
+    excuted_datetime timestamp(6)  DEFAULT NULL,
+    created_at   timestamp(6)  NOT NULL,
+    updated_at   timestamp(6) DEFAULT NULL,
+
+    CONSTRAINT fk_job_lists_to_robots FOREIGN KEY (robot_id) REFERENCES robots (robot_id) ON DELETE CASCADE,
+);
+
+CREATE TABLE jobs
+(
+    job_id        bigint      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    joblist_id   bigint  NOT NULL,
+    command VARCHAR(30)  NOT NULL,
+    parameter   VARCHAR(30) default NULL,
+    activation      TINYINT      NOT NULL,
+
+    CONSTRAINT fk_jobs_to_job_lists FOREIGN KEY (joblist_id) REFERENCES job_lists (joblist_id) ON DELETE CASCADE,
+);
+
+
