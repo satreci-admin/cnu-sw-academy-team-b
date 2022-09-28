8fee03ce639eca79d004956acfaeec91c4afc9f8
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-09-28 15:31:08 +0900

    feat : 로봇 엔티티 추가

---
+++ src/main/java/kr/ac/cnu/swacademy/simplerpa/entity/RobotEntity.java
@@ -0,0 +1,54 @@
+package kr.ac.cnu.swacademy.simplerpa.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "robots")
+public class RobotEntity extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "robot_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(length = 30)
+    private String address;
+    @NotNull
+    @Column(length = 30)
+    private String user;
+    @NotNull
+    @Column(length = 30)
+    private String password;
+
+
+    @Builder
+    public RobotEntity(String address, String user, String password) {
+        this.address = address;
+        this.user = user;
+        this.password = password;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+        this.setUpdateAt(LocalDateTime.now());
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+        this.setUpdateAt(LocalDateTime.now());
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        this.setUpdateAt(LocalDateTime.now());
+    }
+}
