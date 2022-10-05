8495009a97476126f4f0558808ade3b93a33622c
si hwan <pum005@naver.com>
2022-10-05 22:01:45 +0900

    refactor : JobListRepository 제거

--- src/main/java/kr/ac/cnu/swacademy/simplerpa/repository/JobListRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.cnu.swacademy.simplerpa.repository;
-
-import kr.ac.cnu.swacademy.simplerpa.entity.JobListEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobListRepository extends JpaRepository<JobListEntity, Long> {
-}
