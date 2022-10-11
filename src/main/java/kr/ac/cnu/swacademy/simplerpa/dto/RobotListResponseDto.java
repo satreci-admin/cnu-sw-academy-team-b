7954db6473813aa2b5b25225d79c311aa39fa13c
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-11 19:57:39 +0900

    feat : RobotListResponseDto에 getter 추가

--- src/main/java/kr/ac/cnu/swacademy/simplerpa/dto/RobotListResponseDto.java
+++ src/main/java/kr/ac/cnu/swacademy/simplerpa/dto/RobotListResponseDto.java
@@ -1,7 +1,9 @@
 package kr.ac.cnu.swacademy.simplerpa.dto;
 
 import kr.ac.cnu.swacademy.simplerpa.entity.RobotEntity;
+import lombok.Getter;
 
+@Getter
 public class RobotListResponseDto {
     private String address;
     private Long id;
