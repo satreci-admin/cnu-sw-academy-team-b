9ee2b0c1956f4d3d5d07821d37bdd29988091bf7
KIMA <57066971+Kim-AYoung@users.noreply.github.com>
2022-10-20 15:51:36 +0900

    feature: 작업명세서 조회/상세조회 api 구현 (#20)

--- src/main/java/kr/ac/cnu/swacademy/simplerpa/controller/api/JobDescriptorApiController.java
+++ src/main/java/kr/ac/cnu/swacademy/simplerpa/controller/api/JobDescriptorApiController.java
@@ -1,5 +1,7 @@
-package kr.ac.cnu.swacademy.simplerpa.controller;
+package kr.ac.cnu.swacademy.simplerpa.controller.api;
 
+import kr.ac.cnu.swacademy.simplerpa.dto.JobDescriptorListResponseDto;
+import kr.ac.cnu.swacademy.simplerpa.dto.JobDescriptorResponseDto;
 import kr.ac.cnu.swacademy.simplerpa.dto.JobDescriptorSaveRequestDto;
 import kr.ac.cnu.swacademy.simplerpa.dto.JobDescriptorUpdateRequestDto;
 import kr.ac.cnu.swacademy.simplerpa.service.JobDescriptorService;
@@ -7,25 +9,39 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class JobDescriptorApiController {
 
     private final JobDescriptorService jobDescriptorService;
 
-    @PostMapping("/jobdescriptor")
+    @GetMapping("/api/v1/jobdescriptors")
+    public ResponseEntity<List<JobDescriptorListResponseDto>> findAllDesc() {
+        List<JobDescriptorListResponseDto> jobDescriptors = jobDescriptorService.findAllDesc();
+        return ResponseEntity.ok(jobDescriptors);
+    }
+
+    @GetMapping("/api/v1/jobdescriptor/{id}")
+    public ResponseEntity<JobDescriptorResponseDto> findById(@PathVariable Long id) {
+        JobDescriptorResponseDto jobDescriptor = jobDescriptorService.findById(id);
+        return ResponseEntity.ok(jobDescriptor);
+    }
+
+    @PostMapping("/api/v1/jobdescriptor")
     public ResponseEntity<Long> save(@RequestBody JobDescriptorSaveRequestDto requestDto) {
         Long jobDescriptorId = jobDescriptorService.save(requestDto);
         return ResponseEntity.ok(jobDescriptorId);
     }
 
-    @PutMapping("/jobdescriptor/{id}")
+    @PutMapping("/api/v1/jobdescriptor/{id}")
     public ResponseEntity<Long> update(@PathVariable Long id, @RequestBody JobDescriptorUpdateRequestDto requestDto) {
         Long jobDescriptorId = jobDescriptorService.update(id, requestDto);
         return ResponseEntity.ok(jobDescriptorId);
     }
 
-    @DeleteMapping("/jobdescriptor/{id}")
+    @DeleteMapping("/api/v1/jobdescriptor/{id}")
     public ResponseEntity<Long> delete(@PathVariable Long id) {
         Long jobDescriptorId = jobDescriptorService.delete(id);
         return ResponseEntity.ok(jobDescriptorId);
