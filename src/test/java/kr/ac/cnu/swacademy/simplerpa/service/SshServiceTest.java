c2592c2aa60633c6fae21b5834ffe45acb116aaf
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-26 12:43:29 +0900

    test : 잘못된 명령어 전송했을 때와 잘못된 로봇 정보를 주었을 때 에러를 반환하는지 테스트

--- src/test/java/kr/ac/cnu/swacademy/simplerpa/service/SshServiceTest.java
+++ src/test/java/kr/ac/cnu/swacademy/simplerpa/service/SshServiceTest.java
@@ -1,31 +1,61 @@
 package kr.ac.cnu.swacademy.simplerpa.service;
 
-import com.jcraft.jsch.JSchException;
+import kr.ac.cnu.swacademy.simplerpa.dto.LogOutputDto;
+import kr.ac.cnu.swacademy.simplerpa.dto.LogStatus;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class SshServiceTest {
-
-    private SshService sshService;
+    private Logger log = LoggerFactory.getLogger("logback");
 
     @Test
-    void 원격pc에_접속하고_명령어를_실행한다 () {
+    void 원격pc에_접속하고_명령어를_실행하고_info_로그를_반환받는다 () {
+        // given
         String[] commands = new String[3];
         commands[0] = "touch hello";
         commands[1] = "echo \"hello world\" >> hello ";
         commands[2] = "cp hello hello_world";
-        sshService = new SshService("192.168.64.5:22", "skb", "1234", commands);
-        try {
-            sshService.start();
-        }
-        catch (JSchException e){
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        SshService sshService = new SshService("192.168.64.5:22", "skb", "1234", commands);
+
+        // when
+        LogOutputDto outputDto = sshService.start();
+
+        // then
+        log.info("{} : {} ", outputDto.getLogStatus().toString(), outputDto.getMessage());
+        assertThat(outputDto.getLogStatus().toString()).isEqualTo(LogStatus.INFO.toString());
     }
+
+    @Test
+    void 없는_파일을_삭제하는_명령어를_보내_error_로그를_반환받는다 () {
+        // given
+        String[] commands = new String[1];
+        commands[0] = "rm abcd";
+        SshService sshService = new SshService("192.168.64.5:22", "skb", "1234", commands);
+
+        // when
+        LogOutputDto outputDto = sshService.start();
+
+        // then
+        log.info("{} : {} ", outputDto.getLogStatus().toString(), outputDto.getMessage());
+        assertThat(outputDto.getLogStatus().toString()).isEqualTo(LogStatus.ERROR.toString());
+    }
+
+    @Test
+    void 잘못된_로봇_정보를_입력해_error_로그를_반환받는다 () {
+        // given
+        String[] commands = new String[1];
+        commands[0] = "rm abcd";
+        SshService sshService = new SshService("333.333.64.5:22", "12", "1234", commands);
+
+        // when
+        LogOutputDto outputDto = sshService.start();
+
+        // then
+        log.info("{} : {} ", outputDto.getLogStatus().toString(), outputDto.getMessage());
+        assertThat(outputDto.getLogStatus().toString()).isEqualTo(LogStatus.ERROR.toString());
+    }
+
 }
