c92d91ec62b4f6ba3dfc9332a9f385d0a5da0aea
pum005 <108645121+pum005@users.noreply.github.com>
2022-12-04 19:50:04 +0900

    refactor : JobEntityTest -> JobRepositoryTest로 변경 (#48)

--- src/test/java/kr/ac/cnu/swacademy/simplerpa/repository/JobRepositoryTest.java
+++ src/test/java/kr/ac/cnu/swacademy/simplerpa/repository/JobRepositoryTest.java
@@ -1,10 +1,7 @@
-package kr.ac.cnu.swacademy.simplerpa.entity;
+package kr.ac.cnu.swacademy.simplerpa.repository;
 
-import kr.ac.cnu.swacademy.simplerpa.repository.JobDescriptorRepository;
-import kr.ac.cnu.swacademy.simplerpa.repository.JobRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import kr.ac.cnu.swacademy.simplerpa.entity.JobDescriptorEntity;
+import kr.ac.cnu.swacademy.simplerpa.entity.JobEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,100 +13,38 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class JobEntityTest {
+class JobRepositoryTest {
 
     @Autowired
     JobRepository jobRepository;
     @Autowired
     JobDescriptorRepository jobDescriptorRepository;
 
-    private JobEntity jobEntity;
-    private JobDescriptorEntity jobDescriptorEntity;
-
-    @BeforeEach
-    void beforeEach() {
-    }
-
-    @AfterEach
-    void afterEach() {
-
-    }
-
     @Test
-    @DisplayName("작업을 저장한다")
     @Transactional
-    void insert() {
-
+    void 작업을_저장한다() {
+        //given
         JobEntity jobEntity = JobEntity.builder()
                 .command("ls")
                 .parameter("-al")
-                .activation(Boolean.FALSE)
-                .build();
-
-        jobRepository.save(jobEntity);
-
-        JobEntity jobEntity1 = jobRepository.findAll().get(0);
-
-        assertThat(jobEntity1.getCommand()).isEqualTo(jobEntity.getCommand());
-
-    }
-
-    @Test
-    @DisplayName("작업을 하나 조회한다")
-    @Transactional
-    void select() {
-
-        JobEntity jobEntity = JobEntity.builder()
-                .command("ls")
-                .parameter("-al")
-                .jobDescriptorEntity(jobDescriptorEntity)
                 .activation(Boolean.FALSE)
                 .build();
 
         JobEntity savedJobEntity = jobRepository.save(jobEntity);
 
-        JobEntity jobEntity1 = jobRepository.findAll().get(0);
+        //when
+        Optional<JobEntity> foundedJobEntity = jobRepository.findById(jobEntity.getId());
 
-        assertThat(jobEntity1.getCommand()).isEqualTo(savedJobEntity.getCommand());
-
-    }
-
-    @Test
-    @DisplayName("작업을 모두 조회한다")
-    @Transactional
-    void select_all() {
-
-        JobEntity jobEntity1 = JobEntity.builder()
-                .command("ls")
-                .parameter("-al")
-                .jobDescriptorEntity(jobDescriptorEntity)
-                .activation(Boolean.FALSE)
-                .build();
-
-        JobEntity jobEntity2 = JobEntity.builder()
-                .command("cp")
-                .parameter("-r")
-                .jobDescriptorEntity(jobDescriptorEntity)
-                .activation(Boolean.FALSE)
-                .build();
-
-        JobEntity savedJobEntity1 = jobRepository.save(jobEntity1);
-        JobEntity savedJobEntity2 = jobRepository.save(jobEntity2);
-
-        List<JobEntity> jobEntities = jobRepository.findAll();
-
-        assertThat(jobEntities).hasSize(2);
-        assertThat(jobEntities.get(0).getCommand()).isEqualTo(jobEntity1.getCommand());
-        assertThat(jobEntities.get(1).getCommand()).isEqualTo(jobEntity2.getCommand());
-
+        //then
+        assertThat(foundedJobEntity.get()).isEqualTo(savedJobEntity);
 
     }
 
     @Test
-    @DisplayName("작업을 수정한다")
     @Transactional
-    void update() {
+    void 작업을_하나_조회한다() {
 
+        //given
         JobDescriptorEntity jobDescriptorEntity = JobDescriptorEntity.builder()
                 .name("hello")
                 .isRepeat(Boolean.FALSE)
@@ -125,21 +60,91 @@ class JobEntityTest {
         jobDescriptorRepository.save(jobDescriptorEntity);
         JobEntity savedJobEntity = jobRepository.save(jobEntity);
 
+        //when
+        Optional<JobEntity> foundedJobEntity = jobRepository.findById(jobEntity.getId());
 
-        savedJobEntity.setCommand("cp");
-
-
-        Optional<JobEntity> jobEntity1 = jobRepository.findById(savedJobEntity.getId());
-
-        assertThat(jobEntity1).isPresent();
-        assertThat(jobEntity1.get().getCommand()).isEqualTo(savedJobEntity.getCommand());
+        //then
+        assertThat(foundedJobEntity.get()).isEqualTo(savedJobEntity);
 
     }
 
     @Test
-    @DisplayName("작업을 삭제한다")
     @Transactional
-    void delete() {
+    void 작업을_모두_조회한다() {
+
+        //given
+        JobDescriptorEntity jobDescriptorEntity = JobDescriptorEntity.builder()
+                .name("hello")
+                .isRepeat(Boolean.FALSE)
+                .build();
+
+        JobEntity jobEntity1 = JobEntity.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptorEntity(jobDescriptorEntity)
+                .activation(Boolean.FALSE)
+                .build();
+
+        JobEntity jobEntity2 = JobEntity.builder()
+                .command("cp")
+                .parameter("-r")
+                .jobDescriptorEntity(jobDescriptorEntity)
+                .activation(Boolean.FALSE)
+                .build();
+
+        jobDescriptorRepository.save(jobDescriptorEntity);
+        JobEntity savedJobEntity1 = jobRepository.save(jobEntity1);
+        JobEntity savedJobEntity2 = jobRepository.save(jobEntity2);
+
+        //when
+        List<JobEntity> jobEntities = jobRepository.findAll();
+
+        //then
+        assertThat(jobEntities).hasSize(2);
+        assertThat(jobEntities.get(0)).isEqualTo(savedJobEntity1);
+        assertThat(jobEntities.get(1)).isEqualTo(savedJobEntity2);
+
+
+    }
+
+    @Test
+    @Transactional
+    void 작업을_수정한다() {
+
+        //given
+        JobDescriptorEntity jobDescriptorEntity = JobDescriptorEntity.builder()
+                .name("hello")
+                .isRepeat(Boolean.FALSE)
+                .build();
+
+        JobEntity jobEntity = JobEntity.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptorEntity(jobDescriptorEntity)
+                .activation(Boolean.FALSE)
+                .build();
+
+        jobDescriptorRepository.save(jobDescriptorEntity);
+        JobEntity savedJobEntity = jobRepository.save(jobEntity);
+
+        //when
+        savedJobEntity.setCommand("cp");
+
+        //then
+        assertThat(jobEntity).isEqualTo(savedJobEntity);
+        assertThat(jobEntity.getCommand()).isEqualTo(savedJobEntity.getCommand());
+
+    }
+
+    @Test
+    @Transactional
+    void 작업을_삭제한다() {
+
+        //given
+        JobDescriptorEntity jobDescriptorEntity = JobDescriptorEntity.builder()
+                .name("hello")
+                .isRepeat(Boolean.FALSE)
+                .build();
 
         JobEntity jobEntity = JobEntity.builder()
                 .command("ls")
@@ -150,12 +155,12 @@ class JobEntityTest {
 
         JobEntity savedJobEntity = jobRepository.save(jobEntity);
 
-
+        //when
         jobRepository.delete(savedJobEntity);
 
+        //then
         List<JobEntity> jobEntities = jobRepository.findAll();
 
         assertThat(jobEntities).isEmpty();
-
     }
 }
