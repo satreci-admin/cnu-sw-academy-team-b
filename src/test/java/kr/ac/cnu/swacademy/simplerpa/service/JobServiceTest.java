bd09b96fd94c6fdb27d28525ff457f495633da50
si hwan <pum005@naver.com>
2022-12-04 17:28:49 +0900

    test : 작업 Service layer 테스트 추가

--- src/test/java/kr/ac/cnu/swacademy/simplerpa/service/JobServiceTest.java
+++ src/test/java/kr/ac/cnu/swacademy/simplerpa/service/JobServiceTest.java
@@ -1,63 +1,46 @@
 package kr.ac.cnu.swacademy.simplerpa.service;
 
 import kr.ac.cnu.swacademy.simplerpa.dto.JobListResponseDto;
+import kr.ac.cnu.swacademy.simplerpa.dto.JobSaveRequestDto;
+import kr.ac.cnu.swacademy.simplerpa.dto.JobUpdateRequestDto;
 import kr.ac.cnu.swacademy.simplerpa.entity.JobDescriptorEntity;
 import kr.ac.cnu.swacademy.simplerpa.entity.JobEntity;
-import kr.ac.cnu.swacademy.simplerpa.entity.RobotEntity;
 import kr.ac.cnu.swacademy.simplerpa.repository.JobDescriptorRepository;
 import kr.ac.cnu.swacademy.simplerpa.repository.JobRepository;
-import kr.ac.cnu.swacademy.simplerpa.repository.RobotRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class JobServiceTest {
 
-    @Autowired
+    @InjectMocks
     JobService jobService;
 
-    @Autowired
+    @Mock
     JobRepository jobRepository;
 
-    @Autowired
+    @Mock
     JobDescriptorRepository jobDescriptorRepository;
-    @Autowired
-    RobotRepository robotRepository;
-
-    @BeforeEach
-    void setup()
-    {
-        jobRepository.deleteAll();
-        jobDescriptorRepository.deleteAll();
-        robotRepository.deleteAll();
-    }
-    @AfterEach
-    void cleanup()
-    {
-//        jobRepository.deleteAll();
-    }
 
     @Test
     void 작업을_조회한다()
     {
         //given
-        RobotEntity robotEntity = RobotEntity.builder()
-                .address("127.0.0.1")
-                .user("root")
-                .password("1234")
-                .build();
-
         JobDescriptorEntity jobDescriptorEntity = JobDescriptorEntity.builder()
                 .name("hello")
-                .robotEntity(robotEntity)
                 .isRepeat(Boolean.FALSE)
                 .build();
 
@@ -68,28 +51,41 @@ class JobServiceTest {
                 .activation(Boolean.FALSE)
                 .build();
 
-        robotRepository.save(robotEntity);
-        jobDescriptorRepository.save(jobDescriptorEntity);
-        JobEntity savedJobEntity = jobRepository.save(jobEntity);
+        Long id = 1L;
+
+        given(jobRepository.findById(id)).willReturn(Optional.of(jobEntity));
 
         //when
-        Optional<JobListResponseDto> foundJobResponseDto = jobService.findById(savedJobEntity.getId());
+        Optional<JobListResponseDto> foundJobResponseDto = jobService.findById(id);
 
         //then
-        assertThat(foundJobResponseDto).isPresent();
-
+        verify(jobRepository).findById(id);
+        assertThat(foundJobResponseDto.get().getCommand()).isEqualTo(jobEntity.getCommand());
+        assertThat(foundJobResponseDto.get().getActivation()).isEqualTo(jobEntity.getActivation());
+        assertThat(foundJobResponseDto.get().getJobDescriptorEntityId()).isEqualTo(jobEntity.getJobDescriptorEntity().getId());
+        assertThat(foundJobResponseDto.get().getId()).isEqualTo(jobEntity.getId());
     }
+
+    @Test
+    void 작업을_조회할때_입력id의_작업이_없는_경우_예외_테스트()
+    {
+
+        Long id = 1L;
+
+        given(jobRepository.findById(id)).willReturn(Optional.empty());
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> jobService.findById(id))
+                        .isInstanceOf(IllegalArgumentException.class);
+        verify(jobRepository).findById(id);
+    }
+
     @Test
     void 작업을_모두_조회한다()
     {
         //given
         JobDescriptorEntity jobDescriptorEntity1 = JobDescriptorEntity.builder()
                 .name("hello")
-                .isRepeat(Boolean.FALSE)
-                .build();
-
-        JobDescriptorEntity jobDescriptorEntity2 = JobDescriptorEntity.builder()
-                .name("hi")
                 .isRepeat(Boolean.FALSE)
                 .build();
 
@@ -103,24 +99,168 @@ class JobServiceTest {
         JobEntity jobEntity2 = JobEntity.builder()
                 .command("cp")
                 .parameter("-r")
-                .jobDescriptorEntity(jobDescriptorEntity2)
+                .jobDescriptorEntity(jobDescriptorEntity1)
                 .activation(Boolean.FALSE)
                 .build();
 
-        jobDescriptorRepository.save(jobDescriptorEntity1);
-        jobDescriptorRepository.save(jobDescriptorEntity2);
-        JobEntity savedJobEntity1 = jobRepository.save(jobEntity1);
-        JobEntity savedJobEntity2 = jobRepository.save(jobEntity2);
+        Long id = 1L;
+
+        given(jobRepository.findByJobDescriptorEntity(id)).willReturn(List.of(jobEntity1, jobEntity2));
 
         //when
-        List<JobListResponseDto> jobListResponseDtos = jobService.findByJobDescriptorEntity(jobDescriptorEntity1.getId());
+        List<JobListResponseDto> jobListResponseDtos = jobService.findByJobDescriptorEntity(id);
 
         //then
-        assertThat(jobListResponseDtos).hasSize(1);
+        verify(jobRepository).findByJobDescriptorEntity(id);
+        assertThat(jobListResponseDtos).hasSize(2);
         assertThat(jobListResponseDtos.get(0).getId())
-                .isEqualTo(savedJobEntity1.getId());
+                .isEqualTo(jobEntity1.getId());
         assertThat(jobListResponseDtos.get(0).getJobDescriptorEntityId())
-                .isEqualTo(savedJobEntity1.getJobDescriptorEntity().getId());
+                .isEqualTo(jobEntity1.getJobDescriptorEntity().getId());
+        assertThat(jobListResponseDtos.get(1).getId())
+                .isEqualTo(jobEntity2.getId());
+        assertThat(jobListResponseDtos.get(1).getJobDescriptorEntityId())
+                .isEqualTo(jobEntity2.getJobDescriptorEntity().getId());
+    }
+
+    @Test
+    void 작업을_생성한다()
+    {
+
+        JobDescriptorEntity jobDescriptorEntity1 = JobDescriptorEntity.builder()
+                .name("작업명세서1")
+                .isRepeat(false)
+                .build();
+        JobEntity jobEntity1 = JobEntity.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptorEntity(jobDescriptorEntity1)
+                .activation(Boolean.FALSE)
+                .build();
+        JobSaveRequestDto jobSaveRequestDto = JobSaveRequestDto.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptionId(1L)
+                .activation(Boolean.FALSE)
+                .build();
+
+        //given
+        given(jobDescriptorRepository.findById(anyLong())).willReturn(Optional.of(jobDescriptorEntity1));
+        given(jobRepository.save(any(JobEntity.class))).willReturn(jobEntity1);
+
+        //when
+        jobService.create(jobSaveRequestDto);
+
+        //then
+        verify(jobRepository).save(any(JobEntity.class));
+        verify(jobDescriptorRepository).findById(anyLong());
+    }
+
+    @Test
+    void 작업을_생성할때_입력id의_작업명세서가_없는_경우_예외_테스트()
+    {
+        //given
+        JobSaveRequestDto jobSaveRequestDto = JobSaveRequestDto.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptionId(1L)
+                .activation(Boolean.FALSE)
+                .build();
+
+        Long id = 1L;
+        given(jobDescriptorRepository.findById(id)).willReturn(Optional.empty());
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> jobService.create(jobSaveRequestDto))
+                .isInstanceOf(IllegalArgumentException.class);
+        verify(jobDescriptorRepository).findById(id);
+
+    }
+
+    @Test
+    void 작업을_수정한다()
+    {
+
+        JobDescriptorEntity jobDescriptorEntity1 = JobDescriptorEntity.builder()
+                .name("작업명세서1")
+                .isRepeat(false)
+                .build();
+        JobEntity jobEntity1 = JobEntity.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptorEntity(jobDescriptorEntity1)
+                .activation(Boolean.FALSE)
+                .build();
+
+        JobUpdateRequestDto jobUpdateRequestDto = JobUpdateRequestDto.builder()
+                .command("cp")
+                .parameter("-rm")
+                .jobDescriptionId(1L)
+                .activation(Boolean.FALSE)
+                .build();
+
+        Long id = 1L;
+
+        //given
+        given(jobRepository.findById(id)).willReturn(Optional.of(jobEntity1));
+        //when
+        Long updatedId = jobService.update(jobUpdateRequestDto, id);
+
+        //then
+        verify(jobRepository).findById(id);
+        assertThat(updatedId).isEqualTo(jobEntity1.getId());
+        assertThat(jobEntity1.getCommand()).isEqualTo(jobUpdateRequestDto.getCommand());
+        assertThat(jobEntity1.getParameter()).isEqualTo(jobUpdateRequestDto.getParameter());
+        assertThat(jobEntity1.getActivation()).isEqualTo(jobUpdateRequestDto.getActivation());
+    }
+
+    @Test
+    void 작업을_수정할때_입력id의_작업이_없는_경우_예외_테스트()
+    {
+
+        //given
+        JobUpdateRequestDto jobUpdateRequestDto = JobUpdateRequestDto.builder()
+                .command("cp")
+                .parameter("-rm")
+                .jobDescriptionId(1L)
+                .activation(Boolean.FALSE)
+                .build();
+        Long id = 1L;
+        given(jobRepository.findById(id)).willReturn(Optional.empty());
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> jobService.update(jobUpdateRequestDto, id))
+                .isInstanceOf(IllegalArgumentException.class);
+        verify(jobRepository).findById(id);
+
+    }
+
+    @Test
+    void 작업을_삭제한다()
+    {
+
+        JobDescriptorEntity jobDescriptorEntity1 = JobDescriptorEntity.builder()
+                .name("작업명세서1")
+                .isRepeat(false)
+                .build();
+
+        JobEntity jobEntity1 = JobEntity.builder()
+                .command("ls")
+                .parameter("-al")
+                .jobDescriptorEntity(jobDescriptorEntity1)
+                .activation(Boolean.FALSE)
+                .build();
+
+        Long id = 1L;
+
+        //given
+        given(jobRepository.findById(id)).willReturn(Optional.of(jobEntity1));
+
+        //when
+        jobService.delete(id);
+
+        //then
+        verify(jobRepository).findById(id);
     }
 
 }
